284de22461c5a3d65a623a9e1e543c712c8998cd
Peter Hucik <peter.hucik@vacuumlabs.com>
2021-02-10 22:43:42 +0100

    chore: add prettier setup

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import App from './App.svelte';
-import Amplify from '@aws-amplify/core';
-import aws_exports from './aws-exports';
+import App from "./App.svelte";
+import Amplify from "@aws-amplify/core";
+import aws_exports from "./aws-exports";
 
 Amplify.configure(aws_exports || backupConfig);
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: "world",
+  },
 });
 
 export default app;
